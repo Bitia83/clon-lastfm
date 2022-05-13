cb88ee358862ed97753b1a50ff10ca507dc681ce
bitia <bitia_r_24@hotmail.com>
2022-05-13 18:03:57 +0200

    nueva funcion rock

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
-let musicaElements = []; 
-
-fetch('music.json')
+// let musicaElements = []
+//fetch('music.json')
+// .then((response) => response.json())
+// .then((data) => console.log(data))
+//.then((music) => {
+//  let musicaElements = "";
+// for (let i = 0; i < music.length; i++) {
+//   musicaElements += `
+//         <tr>
+//       <td>${i + 1}</td>
+//       <td> ${music[i].artist.name}</td>
+//      <td><b>${music[i].name}</b></td>
+//      <td> ${music[i].listeners}</td>
+//       </tr`;
+//   }
+//  document.getElementById('music-body').innerHTML = musicaElements
+//   });
+// crear una funcion para un lista rock
+function listRock() {
+  fetch('music.json')
     .then((response) => response.json())
-
-    //.then((data) => console.log(data))
-    
-   .then((music) => {
-        for (let i = 0; i < music.length; i++) {
-            musicaElements += `
-            <tr>
-            <td>${i+1}</td>
-            <td> ${music[i].artist.name}</td>
-            <td><b>${music[i].name}</b></td>
-            <td> ${music[i].listeners}</td>
-            </tr`;
-     }
-       document.getElementById('music-body').innerHTML = musicaElements
-       
-
-
+    .then((music) => {
+      let musicaElements = "";
+      let rockArray = music.filter(music => music.genres.includes('rock'));
+      rockArray.forEach(music => {
+        musicaElements += `
+          <tr>
+          <td>${i + 1}</td>
+          <td> ${music[i].artist.name}</td>
+          <td><b>${music[i].name}</b></td>
+          <td> ${music[i].listeners}</td>
+          </tr`;
+      });
+      document.getElementById('music-rock').innerHTML = musicaElements;
     });
+}
 
-  
+;
    // const nombreDeMusica = music.map((music) => music.name);
    // console.log(nombreDeMusica);
 // });
-
-
