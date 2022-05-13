9118cb91d98b950fb22b289441c2cfd7f2d8359e
bitia <bitia_r_24@hotmail.com>
2022-05-13 10:17:20 +0200

    cambio css

--- script.js
+++ script.js
@@ -4,23 +4,27 @@ let musicaElements = ''
 fetch('http://127.0.0.1:5500/music.json')
     .then((response) => response.json())
 
-  // .then((data) => console.log(data))
+    //.then((data) => console.log(data))
     
    .then((music) => {
         for (let i = 0; i < music.length; i++) {
             musicaElements += `
             <tr>
-            <td>${music[i].rank}</td>
+            <td>${i+1}</td>
             <td> ${music[i].artist.name}</td>
             <td><b>${music[i].name}</b></td>
             <td> ${music[i].listeners}</td>
             </tr`;
      }
-        document.getElementById('music-body').innerHTML = musicaElements
+       document.getElementById('music-body').innerHTML = musicaElements
+       
+
+
     });
    
   
    // const nombreDeMusica = music.map((music) => music.name);
    // console.log(nombreDeMusica);
 // });
+
 
