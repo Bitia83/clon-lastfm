141a1c5f285cb46039ee257a3ea5c651bc4f9781
bitia <bitia_r_24@hotmail.com>
2022-05-16 16:00:29 +0200

    ten list

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.getElementById(`hip-hop`).addEventListener(`click`, cargarHipHop);//US 
 document.getElementById(`indie`).addEventListener(`click`, cargarIndie);//US 6: ver lista indie al hacer click en indie
 document.getElementById(`jazz`).addEventListener(`click`, cargarJazz);//US 7: ver lista jazz al hacer click en jazz
 document.getElementById(`reggae`).addEventListener(`click`, cargarReggae);//US 8: lista reggae al hacer click en  reggae
-
+document.getElementById(`top10`).addEventListener(`click`, cargarTenList);
 //Funciones
 //US 1: carga de página inicial. US 2: ver listado Overview. Carga las 50 caciones más escuchadas
 function cargarOverview() {
@@ -17,6 +17,7 @@ function cargarOverview() {
   .then((music) => {
     let musicaElements = "";
     /* Inicio del código para que se ordenen los datos por número de listeners de mayor a menor */
+    
     music.sort((a, b) => {
     if (a.listeners == b.listener) {
       return 0;
@@ -214,7 +215,37 @@ function cargarJazz() {
     });
   };
 
-
+  /* Inicio del código para que se ordenen los datos por número de listeners 
+ de 
+   mayor a menor */
+    
+  function cargarTenList() {
+    fetch('music.json')
+    .then((response) => response.json())
+    .then((music) => {
+      let musicaElements = "";
+      music.sort((a, b) => {
+        return b.listener - a.listeners
+        });
+        let tenListArray = music.filter((music, i) =>  i <= 9);
+          
+         // genres.includes ('reggae'));
+       
+        /* Fin del código para que se ordenen los datos por número de listeners 
+de 
+        mayor a menor */
+           tenListArray.forEach((music, i) => {
+         musicaElements += `
+          <tr>
+          <td>${i + 1}</td>
+          <td> ${music.artist.name}</td>
+          <td><b>${music.name}</b></td>
+          <td> ${music.listeners}</td>
+          </tr`;
+      });
+      document.getElementById('music-body').innerHTML = musicaElements;
+    });
+  };
 
 
    // const nombreDeMusica = music.map((music) => music.name);
