dafb6763d5d6fe4a252b61778bd959eebe285cc1
bitia <bitia_r_24@hotmail.com>
2022-05-16 09:27:54 +0200

    funcion filtrado

--- script.js
+++ script.js
@@ -1,41 +1,40 @@
 // let musicaElements = []
-//fetch('music.json')
-// .then((response) => response.json())
-// .then((data) => console.log(data))
-//.then((music) => {
-//  let musicaElements = "";
-// for (let i = 0; i < music.length; i++) {
-//   musicaElements += `
-//         <tr>
-//       <td>${i + 1}</td>
-//       <td> ${music[i].artist.name}</td>
-//      <td><b>${music[i].name}</b></td>
-//      <td> ${music[i].listeners}</td>
-//       </tr`;
-//   }
-//  document.getElementById('music-body').innerHTML = musicaElements
-//   });
+  fetch('music.json')
+ .then((response) => response.json())
+  // .then((data) => console.log(data))
+ .then((music) => {
+   let musicaElements = "";
+   for (let i = 0; i < music.length; i++) {
+   musicaElements += `
+         <tr>
+       <td>${i + 1}</td>
+       <td> ${music[i].artist.name}</td>
+      <td><b>${music[i].name}</b></td>
+      <td> ${music[i].listeners}</td>
+       </tr>`;
+   }
+ document.getElementById('music-body').innerHTML = musicaElements
+   });
 // crear una funcion para un lista rock
-function listRock() {
+  
   fetch('music.json')
     .then((response) => response.json())
     .then((music) => {
       let musicaElements = "";
-      let rockArray = music.filter(music => music.genres.includes('rock'));
+      let rockArray = music.filter(music => music.genres.includes ('rock'));
       rockArray.forEach(music => {
         musicaElements += `
           <tr>
-          <td>${i + 1}</td>
-          <td> ${music[i].artist.name}</td>
-          <td><b>${music[i].name}</b></td>
-          <td> ${music[i].listeners}</td>
+          <td>${music.rank}</td>
+          <td> ${music.artist.name}</td>
+          <td><b>${music.name}</b></td>
+          <td> ${music.listeners}</td>
           </tr`;
       });
       document.getElementById('music-rock').innerHTML = musicaElements;
     });
-}
 
-;
+
    // const nombreDeMusica = music.map((music) => music.name);
    // console.log(nombreDeMusica);
 // });
